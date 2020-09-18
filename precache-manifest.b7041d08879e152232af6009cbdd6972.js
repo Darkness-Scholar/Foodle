1a6a8840b80d3876a725bc7cb332127c6053d1b5
Tung Hoang <tungxm113@gmail.com>
2020-09-18 10:30:09 +0700

    Updates

---
+++ precache-manifest.b7041d08879e152232af6009cbdd6972.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e5505bd8e3b5efb63b6e6fd44910f79",
+    "url": "/Foodle/index.html"
+  },
+  {
+    "revision": "d3d3256ed176dc212bd1",
+    "url": "/Foodle/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a9a3695c569d1bc0180a",
+    "url": "/Foodle/static/js/2.526eedb9.chunk.js"
+  },
+  {
+    "revision": "219243e74760967cbca211a427f91127",
+    "url": "/Foodle/static/js/2.526eedb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3d3256ed176dc212bd1",
+    "url": "/Foodle/static/js/main.3cee0dcd.chunk.js"
+  },
+  {
+    "revision": "5e3486660165a3c1cc63",
+    "url": "/Foodle/static/js/runtime-main.cb77b628.js"
+  }
+]);
